ea41c0115bdf40c2d26d3b6c218e4f693c422e29
apark102 <32215328+apark102@users.noreply.github.com>
2025-06-18 02:52:39 +0900

    Add files via upload

--- fear_conditioning_model.py
+++ fear_conditioning_model.py
@@ -23,25 +23,33 @@ CS1 = [
 # Weight Initialization
 # Conditioned stimulus to Thalamus
 CS_W_MGv = np.random.rand(16, 8)
+CS_W_MGv /= CS_W_MGv.sum(axis=1, keepdims=True)
 CS_W_MGm = np.random.rand(16, 3)
-
+CS_W_MGm /= CS_W_MGm.sum(axis=1, keepdims=True)
 # Thalamus to Auditory Cortex
 MGv_W = np.random.rand(8, 8)
+MGv_W /= MGv_W.sum(axis=1, keepdims=True)
 MGm_W = np.random.rand(3, 8)
-
+MGm_W /= MGm_W.sum(axis=1, keepdims=True)
 # Thalamus to Amygdala
 MGm_W_Amy = np.random.rand(3, 3)
-
+MGm_W_Amy /= MGm_W_Amy.sum(axis=1, keepdims=True)
 # Auditory Cortex to Amygdala
 AC_W = np.random.rand(8, 3)
-
+AC_W /= AC_W.sum(axis=1, keepdims=True)
 # Weights for second model (CS+US)
 US_CS_W_MGv = np.random.rand(16, 8)
+#US_CS_W_MGv /= CS_W_MGv.sum(axis=1, keepdims=True)
 US_CS_W_MGm = np.random.rand(16, 3)
+#US_CS_W_MGm /= CS_W_MGm.sum(axis=1, keepdims=True)
 US_MGv_W = np.random.rand(8, 8)
+#US_MGv_W /= MGv_W.sum(axis=1, keepdims=True)
 US_MGm_W = np.random.rand(3, 8)
+#US_MGm_W /= MGm_W.sum(axis=1, keepdims=True)
 US_MGm_W_Amy = np.random.rand(3, 3)
+#US_MGm_W_Amy /= MGm_W_Amy.sum(axis=1, keepdims=True)
 US_AC_W = np.random.rand(8, 3)
+#US_AC_W /= AC_W.sum(axis=1, keepdims=True)
 
 # Unconditioned Stimulus Weights
 US_W = np.full(3, 0.4)
@@ -73,7 +81,7 @@ def learning(input_vec, output_vec, weights, learning_rate=0.1):
         if input_vec[i] > input_avg:
             for j in range(len(output_vec)):
                 weights[i][j] += learning_rate * input_vec[i] * output_vec[j]
-    weights /= weights.sum(axis=1, keepdims=True)
+    weights /= weights.sum(axis=0, keepdims=True)
 
 # Feedforward propagation function for CS only
 def forward_pass(CS_input, CS_MGv_weight, CS_MGm_weight, MGv_weight, MGm_weight, AC_weight, MGm_Amy_weight):
@@ -106,7 +114,7 @@ def US_pass(CS_input, CS_MGv_weight, CS_MGm_weight, MGv_weight, MGm_weight, AC_w
     return MGv, MGm, AC, Amy
 
 # Preconditioning training to establish receptive fields
-pretrain_epochs = 50
+pretrain_epochs = 100
 for _ in range(pretrain_epochs):
     for cs_input in CS1:
         MGv, MGm, AC, Amy = forward_pass(cs_input, CS_W_MGv, CS_W_MGm, MGv_W, MGm_W, AC_W, MGm_W_Amy)
@@ -128,7 +136,7 @@ for _ in range(pretrain_epochs):
 # Record preconditioning for graphs
 mgv_pre_RF, mgm_pre_RF, ac_pre_RF, amy_pre_RF = [], [], [], []
 for cs_input in CS1:
-    MGv, MGm, AC, Amy = forward_pass(cs_input, CS_W_MGv, CS_W_MGm, MGv_W, MGm_W, AC_W, MGm_W_Amy)
+    MGv, MGm, AC, Amy = forward_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy)
     mgv_pre_RF.append(MGv)
     mgm_pre_RF.append(MGm)
     ac_pre_RF.append(AC)
@@ -148,19 +156,20 @@ while True:
     except ValueError:
         print("Invalid input.")
 
-cond_epochs = 50
+cond_epochs = 100
 for _ in range(cond_epochs):
     # Goes through every frequency
     for i, cs_input in enumerate(CS1):
         if i == us_index:
             # Runs CS+US forward pass on the frequency paired with the US
             MGv_us, MGm_us, AC_us, Amy_us = US_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy, US_W)
-            learning(cs_input, MGv_us, US_CS_W_MGv)
+            #learning(cs_input, MGv_us, US_CS_W_MGv)
             learning(cs_input, MGm_us, US_CS_W_MGm)
-            learning(MGv_us, activation_function(MGv_us, US_MGv_W), US_MGv_W)
+            #learning(MGv_us, activation_function(MGv_us, US_MGv_W), US_MGv_W)
             learning(MGm_us, activation_function(MGm_us, US_MGm_W), US_MGm_W)
             learning(AC_us, activation_function(AC_us, US_AC_W), US_AC_W)
             learning(MGm_us, activation_function(MGm_us, US_MGm_W_Amy), US_MGm_W_Amy)
+'''
         else:
             # Only update CS-only pathway for unpaired CS
             MGv_us, MGm_us, AC_us, Amy_us = forward_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy)
@@ -180,23 +189,22 @@ for _ in range(cond_epochs):
         learning(MGm, activation_function(MGm, MGm_W), MGm_W)
         learning(AC, activation_function(AC, AC_W), AC_W)
         learning(MGm, activation_function(MGm, MGm_W_Amy), MGm_W_Amy)
-
+'''
 # Post Conditioning
 mgv_post_RF, mgm_post_RF, ac_post_RF, amy_post_RF = [], [], [], []
 mgv_post_RF_us, mgm_post_RF_us, ac_post_RF_us, amy_post_RF_us = [], [], [], []
 
 # Goes through each frequency and measures final activation output for each 
 for i, cs_input in enumerate(CS1):
+    '''
     MGv, MGm, AC, Amy = forward_pass(cs_input, CS_W_MGv, CS_W_MGm, MGv_W, MGm_W, AC_W, MGm_W_Amy)
     mgv_post_RF.append(MGv)
     mgm_post_RF.append(MGm)
     ac_post_RF.append(AC)
     amy_post_RF.append(Amy)
 
-    if i == us_index:
-        MGv_u, MGm_u, AC_u, Amy_u = US_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy, US_W)
-    else:
-        MGv_u, MGm_u, AC_u, Amy_u = forward_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy)
+    '''
+    MGv_u, MGm_u, AC_u, Amy_u = forward_pass(cs_input, US_CS_W_MGv, US_CS_W_MGm, US_MGv_W, US_MGm_W, US_AC_W, US_MGm_W_Amy)
 
     mgv_post_RF_us.append(MGv_u)
     mgm_post_RF_us.append(MGm_u)
@@ -211,7 +219,7 @@ def plot_module_response(module_name, pre_RF, post_RF, post_US_RF):
     x = list(range(len(CS1)))
     plt.figure(figsize=(10, 5))
     plt.plot(x, pre, label="Pre-conditioning", linestyle='--', marker='o')
-    plt.plot(x, post, label="Post-conditioning (CS only)", linestyle='-', marker='s')
+    #plt.plot(x, post, label="Post-conditioning (CS only)", linestyle='-', marker='s')
     plt.plot(x, post_us, label="Post-conditioning (CS + US)", linestyle='-', marker='^')
     plt.axvline(x=us_index, color='red', linestyle=':', label='US-paired CS')
     plt.title(f"{module_name} Mean Activation")
@@ -227,3 +235,4 @@ plot_module_response("MGv", mgv_pre_RF, mgv_post_RF, mgv_post_RF_us)
 plot_module_response("MGm", mgm_pre_RF, mgm_post_RF, mgm_post_RF_us)
 plot_module_response("Auditory Cortex", ac_pre_RF, ac_post_RF, ac_post_RF_us)
 plot_module_response("Amygdala", amy_pre_RF, amy_post_RF, amy_post_RF_us)
+
